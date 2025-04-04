1f8f675358c8cdc6ebf626759052778d0cb17f32
illfiction <atharva.sonwane@outlook.com>
2025-04-04 16:18:11 +0530

    removed major issue in which duplicate cards were present

--- flop_probability.py
+++ flop_probability.py
@@ -48,67 +48,68 @@ def flop_probability(pocket_cards,flop_cards):
     for card1 in remaining_cards:
         for card2 in remaining_cards:
 
-            total_count += 1 #TOTAL COUNT HAS BEEN INCREMENTED.
+            if len({card1, card2}) < 2:
+                print("##Card1 is equal to card2", card1, card2)
 
-            if countOf({card1, card2}):
-                print("Card1 is equal to card2")
-                break
-
-            user_hand = opened_cards.union(set([card1, card2]))
-            print("NEW CASE with cards:",opened_cards,card1,card2)
-            # set that contains one possible cases with 2 random cards to fulfill 7 cards that are visible to player
-            # use that set to check if 5 best cards for a hand or not in order of hands
-            # 2 diff cards from remaining cards to find probability
-
-
-            is_flush = flush_checker(user_hand)
-            is_straight = (straight_checker(user_hand) != 0)
-            is_royal_straight = (straight_checker(user_hand) == 2)
-            '''
-                used this way just so it is more efficient as flush straight are checked multiple times as there are many sub cases
-                the following if elif ladder is implemented so that in a particular 7 card set only the highest hand win, more efficient
-            '''
-
-            if is_flush and is_royal_straight:
-                hands_tally_counter['Royal Flush'] += 1
-                user_hand_rank = 10
-                print('Royal Flush')
-            elif is_straight and is_flush:
-                hands_tally_counter['Straight Flush'] += 1
-                print('Straight Flush')
-                user_hand_rank = 9
-            elif is_four_of_a_kind(user_hand):
-                hands_tally_counter['Four of a Kind'] += 1
-                print('Four of a Kind')
-                user_hand_rank = 8
-            elif is_full_house(user_hand):
-                hands_tally_counter['Full House'] += 1
-                print('Full House')
-                user_hand_rank = 7
-            elif is_flush:
-                hands_tally_counter['Flush'] += 1
-                print('Flush')
-                user_hand_rank = 6
-            elif is_straight:
-                hands_tally_counter['Straight'] += 1
-                print('Straight')
-                user_hand_rank = 5
-            elif is_three_of_a_kind(user_hand):
-                hands_tally_counter['Three of a Kind'] += 1
-                print('Three of a Kind')
-                user_hand_rank = 4
-            elif is_two_pair(user_hand):
-                hands_tally_counter['Two Pair'] += 1
-                print('Two Pair')
-                user_hand_rank = 3
-            elif is_single_pair(user_hand):
-                hands_tally_counter['Single Pair'] += 1
-                print('Single Pair')
-                user_hand_rank = 2
             else:
-                hands_tally_counter['No Pair'] += 1
-                print('No Pair')
-                user_hand_rank = 1
+                total_count += 1  # TOTAL COUNT HAS BEEN INCREMENTED.
+
+
+                user_hand = opened_cards.union(set([card1, card2]))
+                print("NEW CASE with cards:",opened_cards,card1,card2)
+                # set that contains one possible cases with 2 random cards to fulfill 7 cards that are visible to player
+                # use that set to check if 5 best cards for a hand or not in order of hands
+                # 2 diff cards from remaining cards to find probability
+
+
+                is_flush = flush_checker(user_hand)
+                is_straight = (straight_checker(user_hand) != 0)
+                is_royal_straight = (straight_checker(user_hand) == 2)
+                '''
+                    used this way just so it is more efficient as flush straight are checked multiple times as there are many sub cases
+                    the following if elif ladder is implemented so that in a particular 7 card set only the highest hand win, more efficient
+                '''
+
+                if is_flush and is_royal_straight:
+                    hands_tally_counter['Royal Flush'] += 1
+                    user_hand_rank = 10
+                    print('Royal Flush')
+                elif is_straight and is_flush:
+                    hands_tally_counter['Straight Flush'] += 1
+                    print('Straight Flush')
+                    user_hand_rank = 9
+                elif is_four_of_a_kind(user_hand):
+                    hands_tally_counter['Four of a Kind'] += 1
+                    print('Four of a Kind')
+                    user_hand_rank = 8
+                elif is_full_house(user_hand):
+                    hands_tally_counter['Full House'] += 1
+                    print('Full House')
+                    user_hand_rank = 7
+                elif is_flush:
+                    hands_tally_counter['Flush'] += 1
+                    print('Flush')
+                    user_hand_rank = 6
+                elif is_straight:
+                    hands_tally_counter['Straight'] += 1
+                    print('Straight')
+                    user_hand_rank = 5
+                elif is_three_of_a_kind(user_hand):
+                    hands_tally_counter['Three of a Kind'] += 1
+                    print('Three of a Kind')
+                    user_hand_rank = 4
+                elif is_two_pair(user_hand):
+                    hands_tally_counter['Two Pair'] += 1
+                    print('Two Pair')
+                    user_hand_rank = 3
+                elif is_single_pair(user_hand):
+                    hands_tally_counter['Single Pair'] += 1
+                    print('Single Pair')
+                    user_hand_rank = 2
+                else:
+                    hands_tally_counter['No Pair'] += 1
+                    print('No Pair')
+                    user_hand_rank = 1
 
 
             print("Remaining cards:",remaining_cards)
@@ -187,5 +188,8 @@ def flop_probability(pocket_cards,flop_cards):
 
     print(hands_tally_counter)
     print("Total Count:",total_count)
+
+    final_probability = {key:value * 100 / total_count for key, value in hands_tally_counter.items()}
+    print(final_probability)
 
 
